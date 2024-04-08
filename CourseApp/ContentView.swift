c0588cdd8e21942264757d9b6932cc5228327725
karimamaaoui <maaouika07@gmail.com>
2024-04-09 00:53:45 +0100

    Initial Commit

---
+++ CourseApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CourseApp
+//
+//  Created by karimamaaoui on 4/9/24.
+//  Copyright © 2024 karimamaaoui. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
